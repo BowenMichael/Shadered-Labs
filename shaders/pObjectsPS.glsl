0723d4ab0ccef663f522679e84267f8e885ac89e
Michael Bowen <michael.bowen@mymail.champlain.edu>
2020-11-17 17:07:51 -0500

    Have a version of working shadow map

--- shaders/pObjectsPS.glsl
+++ shaders/pObjectsPS.glsl
@@ -6,10 +6,23 @@
 
 //Varyings
 in vec4 vNormal;
+in vec4 vPosition;
+in vec4 vDepth;
+in vec4 vTexCoord;
+in vec4 vLightSpaceCoord;
 
 //Color Target
 layout (location = 0) out vec4 rtFragColor;
+layout (location = 1) out vec4 rtDepth;
 
 void main(){
+	//Prespective Divide
+	//vec4 ndc = 
+	vec3 screen_space = vLightSpaceCoord.xyz * .5 + .5;
+
+	//vec3 ndc = vDepth.xyz / vDepth.z;
+	vec2 uv = vTexCoord.xy / vTexCoord.z; 
+	rtDepth = vec4(screen_space, vDepth.w );
 	rtFragColor = vNormal;
+	rtFragColor = vPosition;
 }
--- shaders/pObjectsVS.glsl
+++ shaders/pObjectsVS.glsl
@@ -16,20 +16,42 @@ layout (location = 1) in vec3 aNormal;
 //Varyings
 out vec4 vColor;
 out vec4 vNormal;
+out vec4 vPosition;
+out vec4 vDepth;
+out vec4 vTexCoord;
+out vec4 vLightSpaceCoord;
+
+//Global Vars
+//float invMax
+float near = 0.1; 
+float far  = 100.0; 
 
 //Global Structures
 struct sLight
 {
-	vec4 center;
+   vec4 center;
+	vec4 center_world;
+	vec4 center_camera;
+	vec4 center_clip;
     vec4 color;
     float intensity;
 };
+
+void lightInit(out sLight light, in vec3 center, in vec3 color, in float intensity)
+{
+	light.center_world = vec4(center, 1.0);
+	light.center = inverse(uModelMat)*light.center_world;
+	light.center_camera = uViewMat * light.center;
+	light.center_clip = uProjectionMat * light.center_camera;
+	light.color = vec4(color, 1.0);
+	light.intensity = 1/intensity; 
+}
 
 void main(){
 
 		//Position Pipeline
 	mat4 modelViewMat = uViewMat * uModelMat; //Model View Matrix
-	//vec4 pos_world = uModelMat * aPosition; //World Space
+    vec4 pos_world = uModelMat * aPosition; //World Space
 	vec4 pos_camera = modelViewMat * aPosition; //Position in camera Space
 	vec4 pos_clip = uProjectionMat * pos_camera; //Position in Clip Space
 	gl_Position = pos_clip;
@@ -40,9 +62,32 @@ void main(){
 	//vec3 norm_camera = mat3(modelViewMat) * aNormal; //viewSpace Normal Blue Hue
 	vec3 norm_clip = mat3(uProjectionMat) * norm_camera;
 	
+	//Light init
+	sLight pointLight;
+	//lightInit(pointLight, vec3(sin(time), 1.0, cos(time)), vec3(1.0), 1.0);
+	lightInit(pointLight, vec3(0.0, 2.0 + sin(time), 0.0), vec3(1.0), 1.3);
+	
+	//Create Shadow Map
+	//vDepth.w = 1.0/(distance(pos_world.xyz, pointLight.center_world.xyz)* pointLight.intensity);
+	//mat4 lightViewMat = gluLookAt(pointLight.x, pointLight.y, pointLight.z, )
+	//mat4 lightProjectionMat = gluPerspective(15, 1.0, near, far);
+	vDepth.w = distance(pos_clip.xyz, pointLight.center_clip.xyz);
+	
+	//Shade the scenen
+	//Convert view Space coords to light space coords
+	vec4 vLightSpaceCoord = pos_camera / pos_camera.w; //in Light Space
+	
+	//Depth Test
+	//Draw The Scene
+	//vDepth.xyz = pointLight.center_camera.xyz;
+    //vDepth.xyz = pointLight.center_clip.xyz - pos_clip.xyz;
+	//vDepth.w = 1.0/(distance(pos_world.xyz, pointLight.center_world.xyz)* pointLight.intensity);
+	
+	//vTexCoord.xyz = vDepth.xyz *.5 + .5;
 	//InClass
-	gl_Position = pos_clip;
-	vNormal = vec4(norm_clip, 0.);
+	//gl_Position = pos_clip;
+	//vPosition = pos_world;
+	//vNormal = vec4(norm_clip, 0.);
 	
 	
   	//vColor = vec4(-pos_camera.xyz, 0.0);
